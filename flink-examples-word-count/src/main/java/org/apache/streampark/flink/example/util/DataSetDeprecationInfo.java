36ec70002a9c3e97f67d5ff038a3cc97f3573ae1
yanglinwei <1962264482@qq.com>
2025-12-18 16:56:57 +0800

    [Feature] Added wordcount example.

---
+++ flink-examples-word-count/src/main/java/org/apache/streampark/flink/example/util/DataSetDeprecationInfo.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2024 Realfuture. All rights reserved.
+ *
+ * This software is the property of Realfuture and is protected by copyright law.
+ * No part of this software may be reproduced, distributed, or transmitted in any form or by any means,
+ * including photocopying, recording, or other electronic or mechanical methods, without the prior
+ * written permission of the copyright owner.
+ */
+
+package org.apache.streampark.flink.example.util;
+
+/** The info about the deprecation of DataSet API. */
+public class DataSetDeprecationInfo {
+
+    public static final String DATASET_DEPRECATION_INFO =
+            "All Flink DataSet APIs are deprecated since Flink 1.18 and will be removed in a future"
+                    + " Flink major version. You can still build your application in DataSet, but you should move to"
+                    + " either the DataStream and/or Table API. This class is retained for testing purposes."
+                    + " See Also: https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=158866741";
+}
